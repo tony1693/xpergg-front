da65da82a6c65e27318c4cdf90d61d5ca5934f4f
tony1693 <toak_16@hotmail.com>
2024-03-23 21:44:33 +0100

    Create-Avatar

--- src/app/components/avatar/avatar.component.ts
+++ src/app/components/avatar/avatar.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { User } from '../../models/user';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-avatar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './avatar.component.html',
   styleUrl: './avatar.component.css'
 })
 export class AvatarComponent {
+  @Input() user!: User;
+  @Input() public selectedAvatar: string = ''; 
+  @Input() public nameUser: string = ''; 
 
+
+  constructor() {
+  }
 }
