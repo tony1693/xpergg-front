e87bc910c9f9581a986fb040078e36288c355b8b
judit Bardon <juditbardonromero@gmail.com>
2024-03-23 19:20:26 +0000

    created LinkComponent with image and text

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { LinkComponent } from './components/link/link.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, LinkComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
