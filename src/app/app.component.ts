ff93ab4c2e219da9636d762a89d9b17a59d4c4be
judit Bardon <juditbardonromero@gmail.com>
2024-03-24 22:08:02 +0000

    created navbar component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { LinkComponent } from './components/link/link.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, LinkComponent],
+  imports: [CommonModule, RouterOutlet, LinkComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { LinkComponent } from '../link/link.component';
+import { User } from '../../models/user';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [LinkComponent],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
