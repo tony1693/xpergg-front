aa78d47a03edf326c2b56b7bad2aa25f5c44e93f
judit Bardon <juditbardonromero@gmail.com>
2024-03-25 17:18:27 +0000

    added user to check the navbar changes if there is a user

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -10,5 +10,18 @@ import { User } from '../../models/user';
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
-
+  public user: User | null = {
+    user_id: 1,
+    avatar: "assets/avatar/call_of_duty-2.jpg",
+    name: "Judit",
+    email: "jbr@gmail.com",
+    nationality: "Spanish",
+    aboutMe: "test test test",
+    password: "Test123",
+    confirmPassword: "Test123",
+    status: true,
+    platforms: ["PS4, PC"],
+    genres: ["Sports, action"]
+  }
+  
 }
