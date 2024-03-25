016c4e3d60e9c1a1a3d73b481d507e3dece31a8a
judit Bardon <juditbardonromero@gmail.com>
2024-03-25 20:05:43 +0000

    created confirmation modal to change password

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { LinkComponent } from './components/link/link.component';
+import { ConfirmationModalComponent } from './components/confirmation-modal/confirmation-modal.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, LinkComponent],
+  imports: [CommonModule, RouterOutlet, LinkComponent, ConfirmationModalComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,5 +1,4 @@
 export class User {
-   
         public user_id!: number;
         public avatar!: string;
         public name!: string;
@@ -8,22 +7,22 @@ export class User {
         public aboutMe!: string;
         public password!: string;
         public confirmPassword!: string;
-        public status!: boolean;
-        public platforms!: string[];
-        public genres!: string[];
+        public status!: Boolean;
+        public platforms!: String[];
+        public genres!: String[];
       
         constructor(user_id: number, name: string, email: string) {
-          this.user_id = user_id;
-          this.name = name;
-          this.email = email;
-          this.avatar = '';
-          this.nationality = '';
-          this.aboutMe = '';
-          this.password = '';
-          this.confirmPassword = '';
-          this.status = false;
-          this.platforms = [];
-          this.genres = [];
+          this.user_id = user_id,
+          this.name = name,
+          this.email = email,
+          this.avatar = '',
+          this.nationality = '',
+          this.aboutMe = '',
+          this.password = '',
+          this.confirmPassword = '',
+          this.status = false,
+          this.platforms = [],
+          this.genres = []
         }
       }
       
