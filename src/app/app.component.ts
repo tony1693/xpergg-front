056b236057dbc19fdbe3d2bc5c0317c9b9b5a5fc
= <=>
2024-03-25 20:05:19 +0100

    subo footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { LinkComponent } from './components/link/link.component';
 import { TrendingNewsComponent } from './components/trending-news/trending-news.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, LinkComponent, TrendingNewsComponent],
+  imports: [CommonModule, RouterOutlet, LinkComponent, TrendingNewsComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
