b5958ac68dc8ea4762a9d6142bea8b859c176e51
MiguelMO2311 <mmeneses73@gmail.com>
2024-03-27 19:19:32 +0100

    primer container landingPage

--- src/app/pages/landing/landing.component.ts
+++ src/app/pages/landing/landing.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { HeaderComponent } from "../../components/header/header.component";
 
 @Component({
-  selector: 'app-landing',
-  standalone: true,
-  imports: [RouterLink],
-  templateUrl: './landing.component.html',
-  styleUrl: './landing.component.css'
+    selector: 'app-landing',
+    standalone: true,
+    templateUrl: './landing.component.html',
+    styleUrl: './landing.component.css',
+    imports: [RouterLink, HeaderComponent]
 })
 export class LandingComponent {
 
