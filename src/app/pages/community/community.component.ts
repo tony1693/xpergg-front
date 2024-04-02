6f0a9761b01ce10270d79683d3d3fa4d9f289002
judit Bardon <juditbardonromero@gmail.com>
2024-04-02 20:20:26 +0100

    gave space to links and positioned navbar in pages

--- src/app/pages/community/community.component.ts
+++ src/app/pages/community/community.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { PlatformCardComponent } from '../../components/platform-card/platform-card.component';
+import { NavbarComponent } from '../../components/navbar/navbar.component';
 
 @Component({
   selector: 'app-community',
   standalone: true,
-  imports: [PlatformCardComponent],
+  imports: [PlatformCardComponent, NavbarComponent],
   templateUrl: './community.component.html',
   styleUrl: './community.component.css',
 })
