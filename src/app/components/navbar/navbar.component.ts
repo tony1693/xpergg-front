51f165bf3726977c129acac761bada324ade61a1
judit Bardon <juditbardonromero@gmail.com>
2024-04-05 19:14:15 +0100

    changed position of Login modal

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -22,17 +22,5 @@ import { FormLoginNavbarComponent } from '../form-login-navbar/form-login-navbar
   styleUrl: './navbar.component.css',
 })
 export class NavbarComponent {
-  public user: User | null = {
-    user_id: 1,
-    avatar: 'assets/avatar/call_of_duty-2.jpg',
-    name: 'Judit',
-    email: 'jbr@gmail.com',
-    nationality: 'Spanish',
-    aboutMe: 'test test test',
-    password: 'Test123',
-    confirmPassword: 'Test123',
-    status: true,
-    platforms: ['PS4, PC'],
-    genres: ['Sports, action'],
-  };
+  public user: User | null = null;
 }
