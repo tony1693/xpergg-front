cd06da71bceca856d3f73c454eb86365c2855d63
judit Bardon <juditbardonromero@gmail.com>
2024-04-06 19:12:49 +0100

    added routing to Init from Navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -5,6 +5,7 @@ import { UsersListComponent } from '../users-list/users-list.component';
 import { CommonModule } from '@angular/common';
 import { RouterLink, RouterModule } from '@angular/router';
 import { FormLoginNavbarComponent } from '../form-login-navbar/form-login-navbar.component';
+import { AvatarComponent } from '../avatar/avatar.component';
 
 @Component({
   selector: 'app-navbar',
@@ -16,12 +17,25 @@ import { FormLoginNavbarComponent } from '../form-login-navbar/form-login-navbar
     RouterModule,
     FormLoginNavbarComponent,
     RouterLink,
+    AvatarComponent,
   ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
 })
 export class NavbarComponent {
-  public user: User | null = null;
+  public user: User | null = {
+    user_id: 1,
+    avatar: 'assets/avatar/call_of_duty-2.jpg',
+    name: 'Judit',
+    email: 'jbr@gmail.com',
+    nationality: 'Spanish',
+    aboutMe: 'test test test',
+    password: 'Test123',
+    confirmPassword: 'Test123',
+    status: true,
+    platforms: ['PS4, PC'],
+    genres: ['Sports, action'],
+  };
 
   logOut() {
     console.log('Cerrando sesión');
