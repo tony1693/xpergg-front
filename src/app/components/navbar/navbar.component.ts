ab0f0bf7778378c10c9dd035c078d3664f40795e
judit Bardon <juditbardonromero@gmail.com>
2024-04-07 18:28:21 +0100

    changed color to avatar name

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -25,7 +25,19 @@ import { LinkWithoutPageComponent } from '../link-without-page/link-without-page
   styleUrl: './navbar.component.css',
 })
 export class NavbarComponent {
-  public user: User | null = null;
+  public user: User | null = {
+    user_id: 1,
+    avatar: 'assets/avatar/call_of_duty-2.jpg',
+    name: 'Judit',
+    email: 'jbr@gmail.com',
+    nationality: 'Spanish',
+    aboutMe: 'test test test',
+    password: 'Test123',
+    confirmPassword: 'Test123',
+    status: true,
+    platforms: ['PS4, PC'],
+    genres: ['Sports, action'],
+  };
 
   logOut() {
     console.log('Cerrando sesión');
