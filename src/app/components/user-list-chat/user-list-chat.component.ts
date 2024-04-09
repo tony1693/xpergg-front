4adb753b47189045f2abe0781d48e8d64a7b52b3
judit Bardon <juditbardonromero@gmail.com>
2024-04-09 17:55:32 +0100

    modified line underneath name in participants. Added routing to footer links

--- src/app/components/user-list-chat/user-list-chat.component.ts
+++ src/app/components/user-list-chat/user-list-chat.component.ts
@@ -49,6 +49,19 @@ export class UserListChatComponent implements OnInit {
         platforms: ['Xbox'],
         genres: ['Coches', 'Estrategia'],
       },
+      {
+        user_id: 1,
+        avatar: 'assets/avatar/call_of_duty-2.jpg',
+        name: 'Judit',
+        email: 'jbr@gmail.com',
+        nationality: 'Spanish',
+        aboutMe: 'test test test',
+        password: 'Test123',
+        confirmPassword: 'Test123',
+        status: true,
+        platforms: ['PS4, PC'],
+        genres: ['Sports, action'],
+      },
     ];
   }
 }
