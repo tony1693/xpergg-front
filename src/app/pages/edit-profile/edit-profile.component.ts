0dbd87a08a5265c19ee3ea1f4aa8d98b6a1aff8f
judit Bardon <juditbardonromero@gmail.com>
2024-04-10 21:36:26 +0100

    parte de editar perfil

--- src/app/pages/edit-profile/edit-profile.component.ts
+++ src/app/pages/edit-profile/edit-profile.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { AvatarComponent } from '../../components/avatar/avatar.component';
 
 @Component({
   selector: 'app-edit-profile',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, AvatarComponent],
   templateUrl: './edit-profile.component.html',
-  styleUrl: './edit-profile.component.css'
+  styleUrl: './edit-profile.component.css',
 })
 export class EditProfileComponent {
-
+  @Input() public currentAvatar: string = 'assets/avatar/Paul_2.webp';
 }
