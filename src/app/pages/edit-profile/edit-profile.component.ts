8c7dcaf2b163d3cc9a34be95f738cfb219cd6093
judit Bardon <juditbardonromero@gmail.com>
2024-04-11 19:29:08 +0100

    changes in edit-profile with media query changes for smaller screen

--- src/app/pages/edit-profile/edit-profile.component.ts
+++ src/app/pages/edit-profile/edit-profile.component.ts
@@ -2,11 +2,18 @@ import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { AvatarComponent } from '../../components/avatar/avatar.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ConfirmationModalComponent } from '../../components/confirmation-modal/confirmation-modal.component';
 
 @Component({
   selector: 'app-edit-profile',
   standalone: true,
-  imports: [CommonModule, AvatarComponent, FormsModule, ReactiveFormsModule],
+  imports: [
+    CommonModule,
+    AvatarComponent,
+    FormsModule,
+    ReactiveFormsModule,
+    ConfirmationModalComponent,
+  ],
   templateUrl: './edit-profile.component.html',
   styleUrl: './edit-profile.component.css',
 })
