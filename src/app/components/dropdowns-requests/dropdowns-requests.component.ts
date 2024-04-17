8b81563e6fcaf2e1344e3998caa5dd8040dbc59c
judit Bardon <juditbardonromero@gmail.com>
2024-04-17 21:58:56 +0100

    changes to request dropdown to fit into navbar

--- src/app/components/dropdowns-requests/dropdowns-requests.component.ts
+++ src/app/components/dropdowns-requests/dropdowns-requests.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { LinkWithoutPageComponent } from '../link-without-page/link-without-page.component';
 
 @Component({
   selector: 'app-dropdowns-requests',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, LinkWithoutPageComponent],
   templateUrl: './dropdowns-requests.component.html',
-  styleUrl: './dropdowns-requests.component.css'
+  styleUrl: './dropdowns-requests.component.css',
 })
 export class DropdownsRequestsComponent {
+  @Input() public optionsVisible: boolean = false;
 
+  notificationsCount: number = 0;
+
+  toggleContent() {
+    this.optionsVisible = !this.optionsVisible;
+  }
+
+  //Método para añadir notificaciones
+
+  getNotifications() {
+    this.notificationsCount += 1;
+  }
 }
