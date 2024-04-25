655d9d6e33b1a97e8b5d711052d8feb05ca4c27f
judit Bardon <juditbardonromero@gmail.com>
2024-04-25 22:21:25 +0100

    añado reload de pagina hilos

--- src/app/components/dropdowns-threads/dropdowns-threads.component.ts
+++ src/app/components/dropdowns-threads/dropdowns-threads.component.ts
@@ -47,7 +47,9 @@ export class DropdownsThreadsComponent {
         next: (data) => {
           console.log(data);
           this.message = 'Hilo creado correctamente';
-          this.threadsService;
+          setTimeout(() => {
+            location.reload();
+          }, 1);
         },
         error: (error) => {
           console.log(error);
